680bdea86ed9c89baa4238aed931f84c43bfebe8
nhatminhdao <nhatminhdao@gmail.com>
2024-07-22 23:11:33 +0000

    first

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/hello") do
+erb (:hello)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get ("/square/new") do
+ @square = @params.fetch(:square).to_f.square
 end
